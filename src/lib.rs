8f7059b68bcc69fec1e2029b14a03931c95f95f8
Colin Ogoo <ogoo.colin@gmail.com>
2024-07-25 22:38:37 +0000

    chore(1.3): 1.3

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 #[cfg(test)]
 mod tests {
+    use bs58;
     use solana_sdk::{self, signature::Keypair, signer::Signer};
+    use std::io::{self, BufRead};
 
     #[test]
     fn keygen() {
@@ -19,4 +21,42 @@ mod tests {
 
     #[test]
     fn transfer_sol() {}
+
+    #[test]
+    fn base58_to_wallet() {
+        println!("Input your private key as a wallet file byte array:");
+        let stdin = io::stdin();
+        let base58 = stdin
+            .lock()
+            .lines()
+            .next()
+            .expect("Failed to read input")
+            .expect("Failed to parse input");
+
+        println!("Your wallet file is:");
+        let wallet = bs58::decode(base58)
+            .into_vec()
+            .expect("Failed to decode base58");
+
+        println!("{:?}", wallet);
+    }
+
+    #[test]
+    fn test_wallet_to_base58() {
+        println!("Input your private key as a wallet file byte array:");
+        let stdin = std::io::stdin();
+        let wallet: Vec<u8> = stdin
+            .lock()
+            .lines()
+            .next()
+            .expect("Failed to read input")
+            .expect("Failed to parse input")
+            .trim_matches(['[', ']'])
+            .split(',')
+            .map(|s| s.trim().parse().expect("Failed to parse byte"))
+            .collect();
+        println!("your private key is:");
+        let base58 = bs58::encode(wallet).into_string();
+        println!("{:?}", base58);
+    }
 }
